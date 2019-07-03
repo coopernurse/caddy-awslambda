42aebd9735d0e007d479e350cd85431fb0c78964
James Cooper <james@bitmechanic.com>
2019-07-03 06:38:12 -0700

    test: use GO111MODULE

--- test_all.sh
+++ test_all.sh
@@ -2,5 +2,7 @@
 
 set -e
 
+export GO111MODULE=on
+go get github.com/caddyserver/caddy
 go test -v -covermode=count -coverprofile=coverage.out
 $GOPATH/bin/goveralls -coverprofile=coverage.out -service=travis-ci -repotoken $COVERALLS_TOKEN
